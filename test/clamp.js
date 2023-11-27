babd3bfb1e97f76ccf448a88004eedbcb02b54ef
Jouka <jahponen@gmail.com>
2023-11-27 17:57:58 +0200

    lisätty coverage työkalu

--- test/clamp.js
+++ test/clamp.js
@@ -13,14 +13,17 @@ describe('clamp.js general tests', () => {
         assert.strictEqual(clamp(numberBelowLower, lower, upper), lower);
     });
 
+    
     it('checks when number is above upper bound', () => {
         assert.strictEqual(clamp(numberAboveUpper, lower, upper), upper);
     });
+    
 
     it('checks when number is exactly at lower bound', () => {
         assert.strictEqual(clamp(lower, lower, upper), lower);
     });
-
+    
+    
     it('checks when number is exactly at upper bound', () => {
         assert.strictEqual(clamp(upper, lower, upper), upper);
     });
@@ -28,6 +31,7 @@ describe('clamp.js general tests', () => {
     it('checks when number is inside the bound values', () => {
         assert.strictEqual(clamp(numberWithinBounds, lower, upper), numberWithinBounds);
     });
+    
 
     it('checks for exception when upper bound is below lower bound', () => {
         expect(() => clamp(numberWithinBounds, upper, lower).to.throw(Error, 'Function throws an error correctly'))
--- test/isEmpty.js
+++ test/isEmpty.js
@@ -2,15 +2,61 @@ import { assert } from 'chai';
 import isEmpty from '../src/isEmpty.js';
 
 describe('isEmtpy.js general tests', () => {
-    it('should return true when value is empty', () => {
+    it('should return true for null value', () => {
         assert.isTrue(isEmpty(null));
+    });
+
+    it('should return true for true', () => {
         assert.isTrue(isEmpty(true));
+    });
+
+    it('should return true for NaN', () => {
         assert.isTrue(isEmpty(NaN));
+    });
+
+    it('should return true for undefined', () => {
         assert.isTrue(isEmpty(undefined));
+    });
+
+    it('should return true for 0', () => {
         assert.isTrue(isEmpty(0));
+    });
+
+    it('should return true for empty array', () => {
         assert.isTrue(isEmpty([]));
+    });
+
+    it('should return true for empty object', () => {
         assert.isTrue(isEmpty({}));
-        assert.isTrue(new Set());
-        assert.isTrue(new Map());
+    });
+
+    it('should return true for empty Set', () => {
+        assert.isTrue(isEmpty(new Set()));
+    });
+
+    it('should return true for empty Map', () => {
+        assert.isTrue(isEmpty(new Map()));
+    });
+
+    it('should return true for custom prototype', () => {
+        const customObject = Object.create(null);
+        assert.isTrue(isEmpty(customObject));
+    });
+
+    it('should return true for custom prototype with no value', () => {
+        const customObject = Object.create({key : null});
+        assert.isTrue(isEmpty(customObject));
+    });
+
+    it('should return false for non-empty array', () => {
+        assert.isFalse(isEmpty([1, 2, 3]));
+    });
+
+    it('should return false for non-empty string', () => {
+        assert.isFalse(isEmpty("test string"));
+    });
+
+    it('should return false for non-empty object', () => {
+        assert.isFalse(isEmpty({key : "value"}));
     });
 });
